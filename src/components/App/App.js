9091fd506dbdbd2e51066945b0174e7ca8c129a6
Kirill <kirillgel1os@gmail.com>
2016-03-06 22:18:33 +0100

    view on github button

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,6 +5,10 @@ export default class App extends Component {
         return (
             <div>
                 {this.props.children}
+                <a href="https://github.com/gel1os/ReactMinesweeper" target="_blank"
+                   className="view-github btn btn-default">
+                    <i className="fa fa-github"></i> View on GitHub
+                </a>
             </div>
         )
     }

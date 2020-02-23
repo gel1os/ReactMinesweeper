b63529c63cb3818e11493c9821b4401272c23136
Kirill Trukhanovich <kirillgel1os@gmail.com>
2020-02-23 20:07:56 +0100

    add server-side rendering

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,2 +1,15 @@
+import React from 'react';
+import { StaticRouter, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux'
 import App from './App';
+
+export const getSSRTemplate = (store) =>
+  <Provider store={store}>
+    <StaticRouter>
+      <Switch>
+        <Route path="*" component={App} />
+      </Switch>
+    </StaticRouter>
+  </Provider>
+
 export default App;

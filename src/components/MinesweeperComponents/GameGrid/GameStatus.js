ae5358865425dd569af6e2a500c5e1de0c70dff0
Kirill Trukhanovich <kirillgel1os@gmail.com>
2020-02-29 20:44:20 +0100

    add dynamic changes of smiles after user's interaction with grid

--- src/components/MinesweeperComponents/GameGrid/GameStatus.js
+++ src/components/MinesweeperComponents/GameGrid/GameStatus.js
@@ -7,6 +7,9 @@ class GameStatus extends Component {
   render() {
     const { gameState } = this.props;
 
+    const isWin = gameState.finished && gameState.win;
+    const isLose = gameState.finished && !gameState.win;
+
     return (
       <div>
         <div className="status-wrapper">
@@ -15,8 +18,10 @@ class GameStatus extends Component {
             {gameState.started ? <Timer /> : <span>0</span>}
           </div>
           <div className="game-result">
-            <img src="icons/smile.svg" className={gameState.finished && gameState.win ? '' : 'hidden'} alt="smile" />
-            <img src="icons/frown.svg" className={gameState.finished && !gameState.win ? '' : 'hidden'} alt="frown" />
+            <img src="icons/smile.svg" className={`smile-icon ${gameState.finished ? 'hidden' : ''}`} alt="smile" />
+            <img src="icons/surprised.svg" className={`surprised-icon ${gameState.finished ? 'hidden' : ''}`} alt="surprised smile" />
+            <img src="icons/cool.svg" className={isWin ? '' : 'hidden'} alt="cool smile in sunglasses" />
+            <img src="icons/frown.svg" className={isLose ? '' : 'hidden'} alt="frown" />
           </div>
           <div className="flags-left">
             <img src="icons/flag.svg" alt="flag" />

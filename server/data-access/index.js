4e07f90b22a5f105b50ced01987607d62f125cb5
Kirill <kirillgel1os@gmail.com>
2021-01-22 23:52:17 +0100

    pass db-port

--- server/data-access/index.js
+++ server/data-access/index.js
@@ -7,6 +7,7 @@ dotenv.config();
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,

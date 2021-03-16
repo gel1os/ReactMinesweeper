efb8d1adb7383bd5d2eb159e55a925332e805895
Kirill Trukhanovich <kirillgel1os@gmail.com>
2021-03-16 23:43:36 +0100

    rewrite Cell and GameComplexity to functional components

--- client/components/MinesweeperComponents/GameComplexity/GameComplexity.js
+++ client/components/MinesweeperComponents/GameComplexity/GameComplexity.js
@@ -1,26 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {complexities} from 'client/utils/constants.js'
 
-export default class GameComplexity extends Component {
-  render() {
-    const {gameSettings, changeGameComplexity} = this.props;
-    return (
-      <div className="complexity-wrapper">
-        <div className="select-complexity">Select complexity:</div>
-        {complexities.map((complexity, i) =>
-          <div className="radio" key={i}>
-            <label>
-              <input
-                type="radio"
-                value={complexity.value}
-                onChange={e => changeGameComplexity(e.target.value)}
-                checked={gameSettings.complexity === complexity.value}
-              />
-              {complexity.label}
-            </label>
-          </div>
-        )}
+const GameComplexity = ({gameSettings, changeGameComplexity}) =>
+  <div className="complexity-wrapper">
+    <div className="select-complexity">Select complexity:</div>
+    {complexities.map((complexity, i) =>
+      <div className="radio" key={i}>
+        <label>
+          <input
+            type="radio"
+            value={complexity.value}
+            onChange={e => changeGameComplexity(e.target.value)}
+            checked={gameSettings.complexity === complexity.value}
+          />
+          {complexity.label}
+        </label>
       </div>
-    );
-  }
-}
+    )}
+  </div>
+
+export default GameComplexity

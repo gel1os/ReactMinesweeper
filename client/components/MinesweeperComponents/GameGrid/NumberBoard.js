7fafffe643efe09add151c361e1b9326698eda0f
Kirill Trukhanovich <kirillgel1os@gmail.com>
2021-03-16 23:52:08 +0100

    rewrite NumberBoard to functional components

--- client/components/MinesweeperComponents/GameGrid/NumberBoard.js
+++ client/components/MinesweeperComponents/GameGrid/NumberBoard.js
@@ -1,26 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {digitsToWordsMap} from 'client/utils/constants';
 
-export default class NumberBoard extends Component {
-  render() {
-    const { number } = this.props;
-    const {hundreds, tens, ones} = this.calculateClasses(number);
-    return (
-      <div className="number-board__wrapper">
-        <div className={`number-board__digit ${hundreds}`}></div>
-        <div className={`number-board__digit ${tens}`}></div>
-        <div className={`number-board__digit ${ones}`}></div>
-      </div>
-    )
-  }
+const NumberBoard = ({number}) => {
+  number = number > 999 ? 999 : number;
+  number = number.toString().padStart(3, '0');
 
-  calculateClasses(number = 0) {
-    number = number > 999 ? 999 : number;
-    number = number.toString().padStart(3, '0');
-    return {
-      hundreds: digitsToWordsMap[number[0]],
-      tens: digitsToWordsMap[number[1]],
-      ones: digitsToWordsMap[number[2]],
-    };
-  }
+  const hundreds = digitsToWordsMap[number[0]];
+  const tens = digitsToWordsMap[number[1]];
+  const ones = digitsToWordsMap[number[2]];
+
+  return (
+    <div className="number-board__wrapper">
+      <div className={`number-board__digit ${hundreds}`}></div>
+      <div className={`number-board__digit ${tens}`}></div>
+      <div className={`number-board__digit ${ones}`}></div>
+    </div>
+  )
 }
+
+export default NumberBoard;

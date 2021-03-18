268990bbd0b9a07c934bc1a93b08694cb2efb95b
Kirill Trukhanovich <kirillgel1os@gmail.com>
2021-03-18 23:48:34 +0100

    refactor HighScoreGrid to use hooks

---
+++ client/components/HighScoreGrid/index.js
@@ -0,0 +1,16 @@
+import { connect } from 'react-redux';
+import {getScore} from 'client/actions/highScoreActions'
+
+import HighScoreGrid from './HighScoreGrid';
+
+const mapStateToProps = ({highScore}) => ({highScore})
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getScore: ({complexity, sortBy, sortDirection, page}) => {
+      dispatch(getScore({complexity, sortBy, sortDirection, page}));
+    }
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(HighScoreGrid);
